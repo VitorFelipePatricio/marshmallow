345fb51ada2977b9017a39066e7275b25c7463fa
VitorFelipePatricio <vitor.felipeee88@gmail.com>
2021-04-23 18:52:02 -0300

    commit

--- pages/home.js
+++ pages/home.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Jeffinho é troll</h1>
+      <h1>Home</h1>
     </div>
   )
 }
